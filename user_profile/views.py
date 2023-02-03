aa900df484a663af1f7b8cd43a20134f4aab33d6
Ozden Atabey <ozdenatabey@hotmail.com>
2023-02-04 00:04:02 +0300

    login bilgilerinin authenticate ile kontrolü saglandi

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 def login_view(request):
     # login olan kullanici direk olarak ana sayfaya gitsin
     context = dict()
+    if request.method == "POST":
+        # print(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        # bu bilgileri dogru aldik mi?
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # login oldugunu kullaniciya belli edelim
+            return redirect('home_view')
     return render(request, 'user_profile/login.html', context)
