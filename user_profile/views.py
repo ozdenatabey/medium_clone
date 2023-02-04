c0bc76c9ec1577a40c49f30692556bb19760de2c
Ozden Atabey <ozdenatabey@hotmail.com>
2023-02-04 16:06:30 +0300

    register sayfası olusturuldu ve navbar kismindakiayarlar duzenlendi

--- user_profile/views.py
+++ user_profile/views.py
@@ -30,3 +30,9 @@ def logout_view(request):
     messages.info(request, f'{request.user.username}Oturum kapatildi.')
     logout(request)
     return redirect('home_view')
+
+def register_view(request):
+    context = dict()
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, 'user_profile/register.html', context)
