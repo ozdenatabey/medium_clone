7129c3added06cc66f406a2172cf9b306a793cb9
Ozden Atabey <ozdenatabey@hotmail.com>
2023-02-05 14:58:47 +0300

    register olmak isteyen kullanicinin kaydinin olup olmadiginin kontrolu yapildi

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def login_view(request):
     # login olan kullanici direk olarak ana sayfaya gitsin
@@ -53,4 +54,15 @@ def register_view(request):
         if password != password_confirm:
             messages.warning(request, "Lütfen password bilgisini doğru giriniz!")
             return redirect('user_profile:register_view')
+        
+        user, created = User.objects.get_or_create(username=email)
+        if not created:
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                messages.success(request, "Daha önce kayıt olmuşsunuz. Ana sayfaya yönlendiriliyorsunuz..")
+                login(request, user)
+                return redirect('home_view')
+            messages.warning(request, f'{email} adresi sistemde kayıtlı ama login olamadınız. Login sayfasına yönlendiriliyorsunuz..')
+            return redirect('user_profile:login_view')
+
     return render(request, 'user_profile/register.html', context)
