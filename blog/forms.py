f78286d31f2594e3d3403cf8f9bca6fbe7f22ab8
Ozden Atabey <ozdenatabey@hotmail.com>
2023-02-21 23:01:16 +0300

    TinyMCE kullanıldı ve PostModelForm içindeki bilgiler kaydedildi

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from tinymce.widgets import TinyMCE
 from .models import BlogPost
 
 class BlogPostModelForm(forms.ModelForm):
     tag = forms.CharField(max_length=200)
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 40,'rows': 20}))
     class Meta:
         model = BlogPost
         fields = [
